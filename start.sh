f856e36875aec7633c45b2fb20d7e4cb899ccd8e
stbhai <121119820+stbhai@users.noreply.github.com>
2022-12-31 14:29:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/MovieStorageWala /MovieStorageWala
+  git clone https://github.com/stbhai/MovieStorageWalaBOT /MovieStorageWala
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MovieStorageWala
