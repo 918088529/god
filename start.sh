4cfa104679c43a9638604d2299591f1faa46940a
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-31 12:57:58 +0530

    Updated V2.0 ⚡

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/lazyindu/MovieStorageWala /MovieStorageWala
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /MovieStorageWala
+fi
+cd /MovieStorageWala
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
