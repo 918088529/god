c6b8691b742cc60c30c3f815f47a84c97871339c
Js62264 <58326525+Js62264@users.noreply.github.com>
2023-01-09 16:23:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,23 +382,21 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-
     url = f'https://gplinks.in/api'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
-              'link': link,
-              'format': 'json'
+    params = {'api': URL_SHORTNER_WEBSITE_API,
+              'url': link,
               }
 
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
+                data = await response.json()
                 if data["status"] == "success":
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://gplinks.in/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://gplinks.in/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
