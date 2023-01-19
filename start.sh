c2714a3da7c2c1826eaf2332be16375ad9c65999
Aayush <bhaskarsharma64047@gmail.com>
2023-01-19 23:40:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/stbhai/MovieStorageWalaBOT /MovieStorageWala
+  git clone https://github.com/Ayush64047/MdiskAdvance /MdiskAdvance 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MovieStorageWala
+  git clone $UPSTREAM_REPO /MdiskAdvance 
 fi
-cd /MovieStorageWala
+cd /MdiskAdvance 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
