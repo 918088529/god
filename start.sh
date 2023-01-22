668c2f3a03adb0c561fb2e9dea0114b4dce6ea15
SHOKDHAR <121552501+918088529@users.noreply.github.com>
2023-01-22 08:26:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ayush64047/MdiskAdvance /MdiskAdvance 
+  git clone https://github.com/918088529/god /god 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MdiskAdvance 
+  git clone $UPSTREAM_REPO /god
 fi
-cd /MdiskAdvance 
+cd /god 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
